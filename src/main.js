23909759ea3186321d49ead66d22f9c7a3c91c2b
Ryan Chapkis <rchapkis@ucsc.edu>
2020-05-03 14:06:03 -0700

    config now allows for physics (game still works as it did before)

--- src/main.js
+++ src/main.js
@@ -2,7 +2,14 @@ let config = {
     type: Phaser.CANVAS,
     width: 640,
     height: 480,
-    scene: [Menu, Play]
+    scene: [Menu, Play],
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true,
+            //gravity: { y: 200 }
+        }
+    }
 }
 
 let game = new Phaser.Game(config);
